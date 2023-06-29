6f313a65023e519ca9c59b568561558a1c5c3780
codecero <boris018790@gmail.com>
2023-06-29 13:15:16 -0400

    nuevo ejerciciio

---
+++ ImprimirTipos/src/TiposDeDatos/Tipos.java
@@ -0,0 +1,28 @@
+package TiposDeDatos;
+
+public class Tipos{
+    public static void main(String[] args) {
+
+
+        //mostrar tipos de datos por consola
+        byte datoByte = 127;
+        System.out.println("Valor de dato Byte: " + datoByte);
+        short datoShort = 32767;
+        System.out.println("Valor de dato Short: " + datoShort);
+        int datoInt = 2147483647;
+        System.out.println("Valor de dato Int: " + datoInt);
+        long datoLong = 3000000000L;
+        System.out.println("Valor de dato Long: " + datoLong);
+        float datoFloat =0.15f;
+        System.out.println("Valor de dato Float: " + datoFloat);
+        double datoDouble =0.45454d;
+        System.out.println("Valor de dato Double: " + datoDouble);
+        char datoChar ='H';
+        System.out.println("Valor de dato Char: " + datoChar);
+        String datoString ="Hola mundo";
+        System.out.println("Valor de dato String"+ datoString);
+        boolean datoBoolean = true;
+        System.out.println("Valor de dato Byte"+ datoBoolean);
+    }
+}
+
